58465bec27e68bf734603ed370e20d2abc5222ac
Cong-Thuan DO <ctdo@gingerminds.fr>
2025-09-03 12:02:45 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div className="app">
+            <div className="padding-config">
+                <h1 className="font-heading text-8xl">Hello World</h1>
+                <p className="font-sans text-16">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                    mollitia quis amet, facilis aspernatur maiores voluptates
+                    architecto. Consequuntur, inventore quisquam.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default App
